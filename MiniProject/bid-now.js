f8436eaf4367e411ff560d7cc0e329041f36cf72
hanifryanas <hanifryanas@gmail.com>
2022-03-04 17:52:30 +0700

    Delete MiniProject directory

--- MiniProject/bid-now.js
+++
@@ -1,9 +0,0 @@
-function bidNow(){
-    if(document.getElementById("login-id").value=="true"){
-        let updatePrice = document.getElementById("mdl-harga-1").value;
-        document.getElementsByClassName("prices")[0].innerHTML=updatePrice;
-    }
-    else{
-        alert("sorry you have to login first");
-    }
-}
--- MiniProject/prices.js
+++
@@ -1,8 +0,0 @@
-function price1(){
-    let price1=document.getElementsByClassName("prices")[0].innerHTML;
-    document.getElementById("mdl-harga-1").value = price1;
-}
-function price2(){
-    let price2=document.getElementsByClassName("prices")[1].innerHTML;
-    document.getElementById("mdl-harga-2").value = price2;
-}
